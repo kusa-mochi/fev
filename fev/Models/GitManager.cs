ccbbdf3bcd4bd33744c46733e4fbe70dd3bc0b9a
kusa-mochi <whoatemyapplepie@gmail.com>
2022-02-12 21:57:52 +0900

    add test run code for git.

--- fev/Models/GitManager.cs
+++ fev/Models/GitManager.cs
@@ -3,31 +3,25 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 
+using fev.Common;
+
 namespace fev.Models
 {
     internal class GitManager
     {
         internal GitManager()
         {
-            try
-            {
-                TestRun();
-            }
-            catch(Exception ex)
-            {
-                System.Windows.MessageBox.Show("error occured !!!");
-                System.Windows.MessageBox.Show(ex.Message);
-            }
+            TestRun();
         }
 
-        internal void TestRun()
+        internal GitOutput RunGitCommand(string gitArguments = "")
         {
             _proc = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = _exe,
-                    Arguments = "",
+                    Arguments = gitArguments,
                     CreateNoWindow = true,
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
@@ -35,23 +29,51 @@ namespace fev.Models
                 }
             };
             _proc.Start();
+
             string output = "";
             string error = "";
-            while(!_proc.StandardOutput.EndOfStream)
+            while (!_proc.StandardOutput.EndOfStream)
             {
                 string line = _proc.StandardOutput.ReadLine();
                 output += line + "\n";
             }
-            while(!_proc.StandardError.EndOfStream)
+            while (!_proc.StandardError.EndOfStream)
             {
                 string line = _proc.StandardError.ReadLine();
                 error += line + "\n";
             }
-            System.Windows.MessageBox.Show(output);
-            System.Windows.MessageBox.Show(error);
+
+            return new GitOutput
+            {
+                Output = output,
+                Error = error
+            };
+        }
+
+        internal void TestRun()
+        {
+            _logManager.AppendLog("begin.");
+            try
+            {
+                GitOutput result = RunGitCommand();
+                System.Windows.MessageBox.Show(result.Output);
+                System.Windows.MessageBox.Show(result.Error);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("error occured !!!");
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+            _logManager.AppendLog("fin.");
+        }
+
+        internal void Checkout(string url)
+        {
+
         }
 
         private Process _proc = null;
         private string _exe = "Resources\\PortableGit\\cmd\\git.exe";
+        private LogManager _logManager = LogManager.GetInstance();
     }
 }
---
+++ fev/Models/GitOutput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fev.Models
+{
+    internal struct GitOutput
+    {
+        internal string Output;
+        internal string Error;
+    }
+}
