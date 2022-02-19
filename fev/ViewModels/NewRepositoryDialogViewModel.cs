f60442abf6264c2b57a73ce6ab635ea8198bf311
kusa-mochi <whoatemyapplepie@gmail.com>
2022-02-19 19:49:40 +0900

    make a dialog for adding a repository.

---
+++ fev/ViewModels/NewRepositoryDialogViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+
+namespace fev.ViewModels
+{
+    public class NewRepositoryDialogViewModel : BindableBase, IDialogAware
+    {
+        public string Title => "New Folder Settings";
+
+        public event Action<IDialogResult> RequestClose;
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
