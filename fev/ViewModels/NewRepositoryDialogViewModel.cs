0f3e5bb0d7e3a38f417267afd1ca2c30ac035ec2
kusa-mochi <whoatemyapplepie@gmail.com>
2022-02-20 13:51:58 +0900

    make a data binding for radio buttons.

--- fev/ViewModels/NewRepositoryDialogViewModel.cs
+++ fev/ViewModels/NewRepositoryDialogViewModel.cs
@@ -5,10 +5,23 @@ using System.Text;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
 
+using fev.Common;
+
 namespace fev.ViewModels
 {
     public class NewRepositoryDialogViewModel : BindableBase, IDialogAware
     {
+        #region Properties
+
+        private RepositoryPlace _repositoryPlace;
+        public RepositoryPlace RepositoryPlace
+        {
+            get { return _repositoryPlace; }
+            set { SetProperty(ref _repositoryPlace, value); }
+        }
+
+        #endregion
+
         public string Title => "New Folder Settings";
 
         public event Action<IDialogResult> RequestClose;
