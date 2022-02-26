368db0510d39e2f97c99765c89f6879ff6297aa3
kusa-mochi <whoatemyapplepie@gmail.com>
2022-02-26 14:23:38 +0900

    make file watcher using .NET class.

---
+++ fwv/Models/FileWatcher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace fwv.Models
+{
+    public class FileWatcher : IDisposable
+    {
+        public event ModifiedEventHandler Modified;
+
+        public FileWatcher(string directoryPath)
+        {
+            _watcher = new FileSystemWatcher(directoryPath);
+            _watcher.Changed += OnChanged;
+            _watcher.Created += OnCreated;
+            _watcher.Deleted += OnDeleted;
+            _watcher.Renamed += OnRenamed;
+        }
+
+        ~FileWatcher()
+        {
+            Dispose();
+        }
+
+        private void OnModified(object sender, string directory, string name)
+        {
+            Modified.Invoke(sender, new ModifiedEventArgs(directory, name));
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            OnModified(sender, Path.GetDirectoryName(e.FullPath), e.Name);
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            OnModified(sender, Path.GetDirectoryName(e.FullPath), e.Name);
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            OnModified(sender, Path.GetDirectoryName(e.FullPath), e.Name);
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            OnModified(sender, Path.GetDirectoryName(e.FullPath), e.Name);
+        }
+
+        public void Dispose()
+        {
+            _watcher?.Dispose();
+        }
+
+        private FileSystemWatcher _watcher = null;
+    }
+}
---
+++ fwv/Models/ModifiedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace fwv.Models
+{
+    public class ModifiedEventArgs : EventArgs
+    {
+        public string FullPath { get; set; }
+        public string Name { get; set; }
+
+        public ModifiedEventArgs(string directory, string name)
+        {
+            FullPath = $"{directory}{name}";
+            Name = name;
+        }
+    }
+
+    public delegate void ModifiedEventHandler(object sender, ModifiedEventArgs args);
+}
