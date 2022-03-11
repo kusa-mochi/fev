591c4a51d7f80ad0440aae88f1cd7fe074e01db6
kusa-mochi <whoatemyapplepie@gmail.com>
2022-03-12 08:23:18 +0900

    make a dialog base for setting user name.

---
+++ fwv/ViewModels/UserNameSettingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Prism.Mvvm;
+
+namespace fwv.ViewModels
+{
+    public class UserNameSettingViewModel : BindableBase
+    {
+        private string _userName = "dummy";
+        public string UserName
+        {
+            get { return _userName; }
+            set { SetProperty(ref _userName, value); }
+        }
+    }
+}
