1b51700777f13ded2bca635dfb531fdb6e0204dd
kusa-mochi <whoatemyapplepie@gmail.com>
2022-03-27 15:43:15 +0900

    modify app settings.

--- fwv/Properties/Settings.Designer.cs
+++ fwv/Properties/Settings.Designer.cs
@@ -23,12 +23,27 @@ namespace fwv.Properties {
             }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("60000")]
         public int WatchInterval {
             get {
                 return ((int)(this["WatchInterval"]));
+            }
+            set {
+                this["WatchInterval"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Repositories {
+            get {
+                return ((string)(this["Repositories"]));
+            }
+            set {
+                this["Repositories"] = value;
             }
         }
     }
